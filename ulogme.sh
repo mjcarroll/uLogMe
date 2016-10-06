41872400e3831388354924728b1f437d8c4480e1
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-06 11:52:12 +0200

    Better script to start collecting data: goes in the good directory, print about the need for the sudo password.

--- ulogme.sh
+++ ulogme.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd ~/.local/share/ulogme/
+echo "Starting 'ulogme.sh' ..."
+
 if [ "$(uname)" == "Darwin" ]; then
-  # This is a Mac
-  ./osx/run_ulogme_osx.sh
+    # This is a Mac
+    ./osx/run_ulogme_osx.sh
 else
-  # Assume Linux
-  sudo echo -n ""
-  sudo ./keyfreq.sh &
-  ./logactivewin.sh
+    # Assume Linux
+    echo "Password is needed please, to run 'keyfreq.sh' as sudo"
+    sudo echo -n ""
+    sudo ./keyfreq.sh &
+    ./logactivewin.sh
 fi
