8ea278631914ea35f59864aa18c1828bd5d667cd
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-07 12:01:28 +0200

    Better title for the *.html pages.

--- keyfreq.sh
+++ keyfreq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# logs the key press frequency over 9 second window. Logs are written 
+# logs the key press frequency over 9 second window. Logs are written
 # in logs/keyfreqX.txt every 9 seconds, where X is unix timestamp of 7am of the
 # recording day.
 
@@ -15,18 +15,18 @@ while true
 do
   showkey > $helperfile &
   PID=$!
-  
-  # work in windows of 9 seconds 
+
+  # work in windows of 9 seconds
   sleep 9
   kill $PID
-  
+
   # count number of key release events
   num=$(cat $helperfile | grep release | wc -l)
-  
+
   # append unix time stamp and the number into file
   logfile="logs/keyfreq_$(python rewind7am.py).txt"
   echo "$(date +%s) $num"  >> $logfile
   echo "logged key frequency: $(date) $num release events detected into $logfile"
-  
+
 done
 
