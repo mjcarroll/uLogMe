17dd266cbcb138c6e4e0a9988fd8792b37b4e67a
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-12 21:19:37 +0200

    I would prefer not to force opening firefox if it was not already running.

--- ulogme_serve.sh
+++ ulogme_serve.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # https://github.com/Naereen/ulogme/
 
+# FIXME adapt this to the path where you stored ulogme.git/
 cd ~/.local/share/ulogme/
 echo "Calling 'ulogme_serve.sh' ..."
 
 port=${1:-8124}
 
-echo -e "Opening 'http://localhost:${port}/' in Firefox ..."
-firefox -new-tab "http://localhost:${port}"/ &
-# xdg-open "http://localhost:${port}"/ &
+if pidof firefox >/dev/null; then
+	echo -e "Opening 'http://localhost:${port}/' in Firefox ..."
+	firefox -new-tab "http://localhost:${port}"/ &
+	# xdg-open "http://localhost:${port}"/ &
+else
+	echo -e "Firefox is not running, by default the uLogMe page will not be opened ..."
+	echo -e "('http://localhost:${port}/' is only opened in a new tab if your Firefox is already running)."
+fi
 
 echo -e "Calling 'python ulogme_serve.py ${port}' ..."
 python ulogme_serve.py "${port}"
