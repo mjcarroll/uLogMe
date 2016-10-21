2b8fcba75f4516c2f2ccd90f72c44420b00af13a
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-21 19:18:39 +0200

    Experimental, trying to add a callback function for the desktop notification, but I failed (it should work better on a purely Ubuntu env, but my mix of Ubuntu and XFCE makes it use xfce4-notifyd instead of Ubuntu's notify-osd...).

--- scripts/notify.py
+++ scripts/notify.py
@@ -6,45 +6,83 @@
 from __future__ import print_function   # Python 2 compatibility
 from __future__ import absolute_import  # Python 2 compatibility
 
+from webbrowser import open as openTab
+from os.path import exists
+
+
+def open_the_ulogme_page(notification, label, args=('localhost, 8124')):
+    """ Open the http://{}:{}/ URL in a new tab of your favorite browser. """
+    print("notify.notify(): open_the_ulogme_page callback function ...")
+    IP, PORT = args
+    ulogeme_url = "http://{}:{}/".format(IP, PORT)
+    print("notify.notify(): Calling 'open(ulogeme_url, new=2, autoraise=True)' ...")
+    return openTab(ulogeme_url, new=2, autoraise=True)
+
+
 PROGRAM_NAME = "uLogMe server (ulogme_serve.py)"
+ICON_PATH    = "pikachu.png"
+
 
 try:
     from gi.repository import Notify
     # One time initialization of libnotify
     Notify.init(PROGRAM_NAME)
 
-    def notify(body, summary=PROGRAM_NAME, icon="dialog-information"):
-        """ Send a notification."""
+    # Loading the icon
+    if exists(ICON_PATH):
+        from gi.repository import GdkPixbuf
+        # Use GdkPixbuf to create the proper image type
+        iconpng = GdkPixbuf.Pixbuf.new_from_file(ICON_PATH)
+    else:
+        iconpng = None
+
+    def notify(body, summary=PROGRAM_NAME,
+               icon="dialog-information",
+               IP="localhost", PORT=8124  # FIXED use this in ulogme_serve.py
+               ):
+        """ Send a notification.
+
+        - icon can be "dialog-information", "dialog-warn", "dialog-error". By default it is set to the 'pikachu.png' image
+        """
         try:
+            # Cf. http://www.devdungeon.com/content/desktop-notifications-python-libnotify
             # Create the notification object
             notification = Notify.Notification.new(
                 summary,
                 body,   # Optional
-                icon  # "dialog-information"  # "dialog-warn", "dialog-error"
+                icon
             )
-
-            # # The notification will have a button that says "Reply to Message".
-            # # my_callback_func is something we will have to define
-            # my_callback_func = lambda x: None
-
-            # notification.add_action(
-            #     "action_click",
-            #     "Reply to Message",
-            #     my_callback_func,
-            #     None  # Arguments
-            # )
 
             # Lowest urgency
             notification.set_urgency(0)
 
-            # Actually show on screen
+            # DONE add duration, lower than 10 seconds (1 second is enough).
+            # XXX it does not seem to work!
+            notification.set_timeout(1)
+
+            # TODO add a Pikachu icom to the notification
+            # Why Pikachu? ALWAYS PIKACHU! http://www.lsv.ens-cachan.fr/~picaro/
+            if iconpng is not None:
+                # Use the GdkPixbuf image
+                notification.set_icon_from_pixbuf(iconpng)
+                notification.set_image_from_pixbuf(iconpng)
+
+            # # XXX add a callback that open the browser tab/page on uLogMe when clicked on it
+            # # The notification will have a button that says "View uLogMe page".
+            # notification.add_action(
+            #     "action_click",
+            #     "View uLogMe page",
+            #     open_the_ulogme_page,
+            #     (IP, PORT)  # Arguments given to open_the_ulogme_page
+            # )
+
+            # Actually show the notification on screen
             notification.show()
 
         # Ugly! XXX Catches too general exception
         except Exception as e:
             print("\nnotify.notify(): Error, notify.notify() failed, with this exception")
             print(e)
-            # print("Exception: dir(e) =", dir(e))  # DEBUG
 
 
 except ImportError:
@@ -54,10 +92,13 @@ except ImportError:
 
     from subprocess import Popen
 
-    def notify(body, summary=PROGRAM_NAME, icon="dialog-information"):
+    def notify(body, summary=PROGRAM_NAME,
+               icon="dialog-information",
+               IP="localhost", PORT=8124  # XXX unused here, notify-send does not accept callback functions
+               ):
         """ Send a fake notification."""
         print("notify.notify(): Warning, desktop notification from Python seems to not be available ...")
-        # print("Notification: '{}', from '{}' with icon '{}'.".format(body, summary, icon))
+        print("Notification: '{}', from '{}' with icon '{}'.".format(body, summary, icon))  # DEBUG
         try:
             print("notify.notify(): Trying to use the command line program 'notify-send' ...")
             if icon:
