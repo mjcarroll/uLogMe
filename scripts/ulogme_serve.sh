0090070108c1f22f7c927c1b9786a21e1f78a56b
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-21 18:51:32 +0200

    Possibly better way to open the uLogMe page in a new tab in your browser.

--- scripts/ulogme_serve.sh
+++ scripts/ulogme_serve.sh
@@ -11,9 +11,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 port="${1:-8124}"
 
 if pidof firefox >/dev/null; then
-	echo -e "Opening 'http://localhost:${port}/' in Firefox ..."
+	echo -e "Opening 'http://localhost:${port}/' in your favorite browser ..."
 	firefox -new-tab "http://localhost:${port}"/ &
-	# xdg-open "http://localhost:${port}"/ &
+    # xdg-open "http://localhost:${port}"/ &  # Generic on Linux
+	# open "http://localhost:${port}"/ &      # Generic on Mac
+    # XXX this should be a better and cross-platform way to do it
+    # python -m webbrowser -t "http://localhost:${port}"/
 else
 	echo -e "Firefox is not running, by default the uLogMe page will not be opened ..."
 	echo -e "('http://localhost:${port}/' is only opened in a new tab if your Firefox is already running)."
