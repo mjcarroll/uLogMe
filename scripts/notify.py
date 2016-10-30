4296b17cc384e6b809f34e9c45f690fde3129981
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-30 10:45:31 +0100

    Refactor 'notify.py', now for every notification it tries to use gi.repository.Notification and if this fails, it uses Popen(...) to 'notify-send'. Should be more stable.

--- scripts/notify.py
+++ scripts/notify.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf8 -*-
-# ulogme_serve.py for https://github.com/Naereen/uLogMe/
+# notify.py for https://github.com/Naereen/uLogMe/
 # MIT Licensed, https://lbesson.mit-license.org/
 #
+"""
+Defines one useful function notify() to (try to) send a desktop notification.
+"""
+
 from __future__ import print_function   # Python 2 compatibility
 from __future__ import absolute_import  # Python 2 compatibility
 
@@ -10,18 +14,21 @@ from webbrowser import open as openTab
 from os.path import exists, join
 from glob import glob
 from random import choice
+from subprocess import Popen
 
 
 # XXX Unused callback function for the notifications
-def open_the_ulogme_page(notification, label, args=('localhost, 8124')):
+def open_the_ulogme_page(notification, label, args=("localhost", 8124)):
     """ Open the http://{}:{}/ URL in a new tab of your favorite browser. """
     print("notify.notify(): open_the_ulogme_page callback function ...")
+    print("open_the_ulogme_page(): notification = {}, label = {}, args = {} ...".format(notification, label, args))
     IP, PORT = args
     ulogme_url = "http://{}:{}/".format(IP, PORT)
     print("notify.notify(): Calling 'open(ulogme_url, new=2, autoraise=True)' ...")
     return openTab(ulogme_url, new=2, autoraise=True)
 
 
+# Constants for the program
 PROGRAM_NAME = "uLogMe server (ulogme_serve.py)"
 ICON_PATH    = join("..", "scripts", "icons", "pikachu.png")
 ICON_PATHS   = glob(join("..", "scripts", "icons", "*.png"))
@@ -37,7 +44,6 @@ try:
         else:
             iconpath = ICON_PATH
         # print("iconpath =", iconpath)  # DEBUG
-        # # print("os.getcwd(): ", os.getcwd())  # DEBUG
         # Loading the icon...
         if exists(iconpath):
             # Use GdkPixbuf to create the proper image type
@@ -48,41 +54,59 @@ try:
         return iconpng
 
 except ImportError:
+    print("\nError, gi.repository.GdkPixbuf seems to not be available, so notification icons will not be available ...")
+    print("On Ubuntu, if you want notification icons to work, install the 'python-gobject' and 'libnotify-bin' packages.")
+    print("(For more details, cf. 'http://www.devdungeon.com/content/desktop-notifications-python-libnotify')")
+
     def load_icon(random=True):
         return None
 
 
+# Trying to import gi.repository.Notify
+has_Notify = False
 try:
     from gi.repository import Notify
     # One time initialization of libnotify
+    Notify.init(PROGRAM_NAME)
+    has_Notify = True
+except ImportError:
+    print("\nError, gi.repository.Notify seems to not be available, so notification will not be available ...")
+    print("On Ubuntu, if you want notifications to work, install the 'python-gobject' and 'libnotify-bin' packages.")
+    print("(For more details, cf. 'http://www.devdungeon.com/content/desktop-notifications-python-libnotify')")
 
-    def notify(body, summary=PROGRAM_NAME,
-               icon="dialog-information",
-               IP="localhost", PORT=8124  # FIXED use this in ulogme_serve.py
-               ):
-        """ Send a notification.
 
-        - icon can be "dialog-information", "dialog-warn", "dialog-error". By default it is set to the 'pikachu.png' image
-        """
-        try:
-            Notify.init(PROGRAM_NAME)
+# Define the first notify function, with gi.repository.Notify
+def notify_gi(body, summary=PROGRAM_NAME,
+              icon=None,
+              IP="localhost", PORT=8124,  # FIXED use this in ulogme_serve.py
+              timeout=5  # In seconds
+              ):
+    """
+    Send a notification, with gi.repository.Notify.
 
-            # Cf. http://www.devdungeon.com/content/desktop-notifications-python-libnotify
-            # Create the notification object
+    - icon can be "dialog-information", "dialog-warn", "dialog-error". By default it is set to the 'pikachu.png' image
+    """
+    try:
+        # Trying to fix a bug:
+        # g-dbus-error-quark: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name :1.5124 was not provided by any .service files (2)
+        Notify.init(PROGRAM_NAME)
+        # XXX maybe the PROGRAM_NAME should be random?
+
+        # Cf. http://www.devdungeon.com/content/desktop-notifications-python-libnotify
+        # Create the notification object
+        if icon is not None:
             notification = Notify.Notification.new(
-                summary,
-                body    # Optional
-                # icon  # XXX Should not indicate it here
+                summary,  # Title of the notification
+                body,     # Optional content of the notification
+                icon      # XXX Should not indicate it here
+            )
+        else:
+            notification = Notify.Notification.new(
+                summary,  # Title of the notification
+                body      # Optional content of the notification
             )
 
-            # Lowest urgency
-            notification.set_urgency(0)
-
-            # DONE add duration, lower than 10 seconds (1 second is enough).
-            # XXX it does not seem to work!
-            notification.set_timeout(1)
-
-            # TODO add a Pikachu icom to the notification
+            # Add a Pikachu icom to the notification
             # Why Pikachu? ALWAYS PIKACHU! http://www.lsv.ens-cachan.fr/~picaro/
             iconpng = load_icon(random=True)
             if iconpng is not None:
@@ -90,53 +114,78 @@ try:
                 notification.set_icon_from_pixbuf(iconpng)
                 notification.set_image_from_pixbuf(iconpng)
 
-            # # XXX add a callback that open the browser tab/page on uLogMe when clicked on it
-            # # The notification will have a button that says "View uLogMe page".
-            # notification.add_action(
-            #     "action_click",
-            #     "View uLogMe page",
-            #     open_the_ulogme_page,
-            #     (IP, PORT)  # Arguments given to open_the_ulogme_page
-            # )
+        # Lowest urgency (LOW, NORMAL or CRITICAL)
+        notification.set_urgency(Notify.Urgency.LOW)
 
-            # Actually show the notification on screen
-            notification.show()
+        # add duration, lower than 10 seconds (5 second is enough).
+        notification.set_timeout(timeout * 1000)
 
-        # Ugly! XXX Catches too general exception
-        except Exception as e:
-            print("\nnotify.notify(): Error, notify.notify() failed, with this exception")
-            print(e)
+        # # XXX add a callback that open the browser tab/page on uLogMe when clicked on it
+        # # The notification will have a button that says "View uLogMe page".
+        # #    "default",
+        # notification.add_action(
+        #     "action_click",
+        #     "View uLogMe page",
+        #     open_the_ulogme_page,
+        #     (IP, PORT)  # Arguments given to open_the_ulogme_page
+        # )
+        # FIXME how to disable on jarvisPRO but keep it on jarvisOld ?
+
+        # Actually show the notification on screen
+        notification.show()
+        return 0
+
+    # Ugly! XXX Catches too general exception
+    except Exception as e:
+        print("\nnotify.notify(): Error, notify.notify() failed, with this exception")
+        print(e)
+        return -1
 
 
-except ImportError:
-    print("\nError, gi.repository.Notify seems to not be available, so notification will not be available ...")
-    print("On Ubuntu, if you want notifications to work, install the 'python-gobject' and 'libnotify-bin' packages.")
-    print("(For more details, cf. 'http://www.devdungeon.com/content/desktop-notifications-python-libnotify')")
-
-    from subprocess import Popen
-
-    def notify(body, summary=PROGRAM_NAME,
+# Define the second notify function, with a subprocess call to 'notify-send'
+def notify_cli(body, summary=PROGRAM_NAME,
                icon="dialog-information",
-               IP="localhost", PORT=8124  # XXX unused here, notify-send does not accept callback functions
+               IP="localhost", PORT=8124,  # XXX unused here, notify-send does not accept callback functions
+               timeout=5  # In seconds
                ):
-        """ Send a fake notification."""
-        print("notify.notify(): Warning, desktop notification from Python seems to not be available ...")
-        print("Notification: '{}', from '{}' with icon '{}'.".format(body, summary, icon))  # DEBUG
+    """
+    Send a notification, with a subprocess call to 'notify-send'.
+    """
+    print("notify.notify(): Warning, desktop notification from Python seems to not be available ...")
+    try:
+        print("notify.notify(): Trying to use the command line program 'notify-send' ...")
+        if icon:
+            Popen(["notify-send", "--expire-time=%s" % (timeout * 1000), "--icon=%s" % (icon), summary, body])
+            print("notify.notify(): A notification have been sent, with summary = %s, body = %s, expire-time = %s and icon = %s." % (summary, body, timeout * 1000, icon))
+        else:
+            Popen(["notify-send", "--expire-time=%s" % (timeout * 1000), summary, body])
+            print("notify.notify(): A notification have been sent, with summary = %s, body = %s and expire-time = %s." % (summary, body, timeout * 1000))
+        return 0
+    # Ugly! XXX Catches too general exception
+    except Exception as e:
+        print("\nnotify.notify(): notify-send : not-found ! Returned exception is %s." % e)
+        return -1
+
+
+# Define the unified notify.notify() function
+def notify(body, summary=PROGRAM_NAME,
+           icon=None,
+           IP="localhost", PORT=8124,  # FIXED use this in ulogme_serve.py
+           timeout=5  # In seconds
+           ):
+    print("Notification: '{}', from '{}' with icon '{}'.".format(body, summary, icon))  # DEBUG
+    if not has_Notify:
+        return notify_cli(body, summary=summary, icon=icon, IP=IP, PORT=PORT, timeout=timeout)
+    else:
         try:
-            print("notify.notify(): Trying to use the command line program 'notify-send' ...")
-            if icon:
-                Popen(['notify-send', summary, body, "--icon=%s" % (icon)])
-                print("notify.notify(): A notification have been sent, with summary = %s, body = %s, and icon = %s." % (summary, body, icon))
-            else:
-                Popen(['notify-send', summary, body])
-                print("notify.notify(): A notification have been sent, with summary = %s, and body = %s." % (summary, body))
-            return 0
-        # Ugly! XXX Catches too general exception
-        except Exception as e:
-            print("notify.notify(): notify-send : not-found ! Returned exception is %s." % e)
-            return -1
+            return_code = notify_gi(body, summary=summary, icon=icon, IP=IP, PORT=PORT, timeout=timeout)
+            if return_code < 0:
+                return_code = notify_cli(body, summary=summary, icon=icon, IP=IP, PORT=PORT, timeout=timeout)
+        except Exception:
+            return_code = notify_cli(body, summary=summary, icon=icon, IP=IP, PORT=PORT, timeout=timeout)
+        return return_code
 
 
-if __name__ == '__main__':
-    # notify("Test body Test body Test body Test body Test body Test body ! With icon=terminal ...", icon="terminal")
+if __name__ == "__main__":
+    notify("Test body Test body Test body Test body Test body Test body ! With icon=terminal ...", icon="terminal")
     notify("Test body Test body Test body Test body Test body Test body ! With random Pokémon icon ...")
