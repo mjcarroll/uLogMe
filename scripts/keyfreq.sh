ac11a72632fd4210d6c5b127936ddc1d63d6e7ed
Lilian Besson <lbesson@ens-cachan.fr>
2016-11-02 22:59:44 +0100

    Simpler way to log keyboard activity: using xinput, no need for sudo password.
    Thanks to https://github.com/karpathy/ulogme/commit/fd2e62e665c49dcb5c94acc559abc696d8951232

--- scripts/keyfreq.sh
+++ scripts/keyfreq.sh
@@ -3,41 +3,92 @@
 # MIT Licensed, https://lbesson.mit-license.org/
 #
 # Logs the key press frequency over 10 second window.
-# Logs are written in logs/keyfreqX.txt every 10 seconds, where X is unix timestamp of 7am of the recording day.
+# Logs are written in ../logs/keyfreq_X.txt every 10 seconds, where X is unix timestamp of 7am of the recording day.
+
+set -o nounset  # Stricter mode
 
 # Use https://bitbucket.org/lbesson/bin/src/master/.color.sh to add colors in Bash scripts
-[ -f ~/.color.sh ] && . ~/.color.sh
 [ -f color.sh ] && . color.sh
 
-LANGUAGE=en
-LANG=en_US.utf8
+# Constants
+readonly LANGUAGE=en
+readonly LANG=en_US.utf8
 
-helperfile="../logs/keyfreqraw.txt"  # temporary helper file
+# Configuration variables
+POLLING_INTERVAL=10
+COMPRESS_LOG_FILE=false  # FIXME is it supported by my version of the UI? Yes
+
+readonly MODIFIER_KEYS="$(xmodmap -pk | grep -iE '\b(shift_[lr]|alt_[lr]|control_[lr]|caps_lock)\b' | cut -f 1 | xargs | tr ' ' '|')"
+
+# Shared memory, instead of a temporary file: more secure, and quicker
+readonly HELPER_FILE=/dev/shm/keyfreqraw
+
 
 mkdir -p ../logs
+last_log_file=""
 
+
+# First message to inform that the script was started correctly
+echo -e "${green}$0 has well been started.${white}"
+nb_virtual_kb=$(xinput | grep 'slave  keyboard' | grep -o 'id=[0-9]*' | cut -d= -f2 | wc -l)
+nb_real_kb=$(xinput | grep 'keyboard.*slave.*keyboard' | grep -v 'Virtual' | wc -l)
+echo -e "  - It will ${red}constantly${white} record the keyboard(s) of your laptop (currently there seems to be ${black}${nb_virtual_kb}${white} virtual keyboard(s) and ${black}${nb_real_kb}${white} real keyboard(s)."
+echo -e "  - It will work in time window of ${red}$POLLING_INTERVAL${white} seconds."
+[ $COMPRESS_LOG_FILE = true ] && echo -e "  - It will regularly compress the log files."
+echo
+
+# Start the main loop
 while true; do
-    # Thanks to https://github.com/Naereen/ulogme/pull/5/
-    showkey | tr -d '0-9' &> "$helperfile" &
-    # PID=$!
+    # Alternative solution, with no need for sudo password, only on Linux
+    # Thanks to @blackdaemon, fd2e62e (https://github.com/karpathy/ulogme/commit/fd2e62e665c49dcb5c94acc559abc696d8951232)
 
-    # work in windows of 10 seconds
-    sleep 10
+    # check each possible keyboard
+    keyboardIds=$(xinput | grep 'slave  keyboard' | grep -o 'id=[0-9]*' | cut -d= -f2)
+    # and grep only the updated ones
+    filesToGrep=''
+    for kbd_id in $keyboardIds; do
+        fileName="${HELPER_FILE}.$kbd_id"
+        # Works in windows of $POLLING_INTERVAL seconds.
+        # Use stdbuf to remove output buffering otherwise it does not log keys before it's terminated by timeout command
+        { (stdbuf -o0 timeout -s 10 $POLLING_INTERVAL xinput test $kbd_id 2>/dev/null `# Grab key events for $POLLING_INTERVAL seconds` \
+            | grep press 2>/dev/null `# Filter out only press events (I am getting doubled release events for some reason)` \
+            | tr -dc '0-9\n' `# Keep only keycodes` \
+            | grep -vE "$MODIFIER_KEYS" `# Remove modifier keys` \
+            | tr -c '\n' '0' `# Replace keycodes with 0 for added security` \
+            ) > "$fileName"; } &
+        filesToGrep+="$fileName "
+    done
 
-    # XXX Find a safer and better way to kill the process
-    # kill $PID
-    kill "$(jobs -rp)" 2>/dev/null
-    wait "$(jobs -rp)" 2>/dev/null
+    wait
 
-    # count number of key release events
-    num=$(grep -c release "$helperfile")
 
-    # append unix time stamp and the number into file
-    logfile="../logs/keyfreq_$(python rewind7am.py).txt"
-    echo "$(date +%s) $num"  >> "$logfile"
-    # only print if $num > 0
+    # Count number of key release events
+    # num=$(grep -c release "$filesToGrep")
+    num=$(cat $filesToGrep | wc -l)
+
+    # Append unix time stamp and the number into file
+    log_file="../logs/keyfreq_$(python rewind7am.py).txt"
+    # Only print and log if $num > 0
     if [ "$num" -gt 0 ]; then
-        echo -e "Logged ${yellow}key frequency${white}: ${magenta}$(date)${white} ${green}${num}${white} release events detected into '${black}${logfile}${white}'"
+        echo "$(date +%s) $num"  >> "$log_file"
+        echo -e "Logged ${yellow}key frequency${white}: ${magenta}$(date)${white} ${green}${num}${white} release events detected into '${black}${log_file}${white}'"
+    fi
+
+
+    if [ "$last_log_file" != "$log_file" ]; then
+        # Optionally compress the log file (remove extraneous 0 key counts)
+        if [ $COMPRESS_LOG_FILE = true ] && [ -s "$last_log_file" ]; then
+            grep -s -v " 0$" -A 1 -B 1 "$last_log_file" \
+                | sort -u \
+                | grep -s -v "^\-\-$" > "${last_log_file}.compressed"
+            echo "${blue}Compressing keyfreq log file${white}: ${yellow}$(ls -hsx ${last_log_file}*)${white}"
+            mv -f -- "${last_log_file}.compressed" "$last_log_file"
+        fi
+        # Create symlink to most recent log file everytime it changes its name,
+        # so we can use something like "tail -F ../logs/keyfreq_today.txt"
+        ln -s -f "$(basename "$log_file")" "../logs/keyfreq_today.txt"
+        last_log_file="$log_file"
+        # XXX I don't use this feature, but alright
     fi
 done
 
--- scripts/ulogme_data.sh
+++ scripts/ulogme_data.sh
@@ -3,13 +3,14 @@
 # MIT Licensed, https://lbesson.mit-license.org/
 
 # Use https://bitbucket.org/lbesson/bin/src/master/.color.sh to add colors in Bash scripts
-[ -f ~/.color.sh ] && . ~/.color.sh
 [ -f color.sh ] && . color.sh
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-echo -e "${red}Password is needed please${white}, to run '${black}keyfreq.sh${white}' with sudo ..."
-sudo echo -n ""
+# echo -e "${red}Password is needed please${white}, to run '${black}keyfreq.sh${white}' with sudo ..."
+# sudo echo -n ""
 
-sudo ./keyfreq.sh &
+# sudo ./keyfreq.sh &
+./keyfreq.sh &
+
 ./logactivewin.sh
