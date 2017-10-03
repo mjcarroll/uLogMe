207739c8ef3df710665d58188a6c01c244201119
Lilian Besson <lbesson@ens-cachan.fr>
2017-10-03 10:13:47 +0200

    Fixing #17 (best I can do).

--- scripts/ulogme_serve.sh
+++ scripts/ulogme_serve.sh
@@ -3,6 +3,7 @@
 # MIT Licensed, https://lbesson.mit-license.org/
 
 # Use https://bitbucket.org/lbesson/bin/src/master/.color.sh to add colors in Bash scripts
+echo "WARNING If you don't see colors correctly, remove the 'color.sh' file in 'uLogMe/scripts' to remove the colors, or modify it to suit your need (if you have a light background for instance)."  # See https://github.com/Naereen/uLogMe/issues/17
 [ -f color.sh ] && . color.sh
 
 echo -e "${yellow}Starting '${black}ulogme_serve.sh${white}' ..."
--- scripts/ulogme_tmux.sh
+++ scripts/ulogme_tmux.sh
@@ -2,9 +2,10 @@
 # ulogme_tmux.sh for https://github.com/Naereen/uLogMe/
 # MIT Licensed, https://lbesson.mit-license.org/
 #
-# Experimental script to start a new tab in a tmux session, launching the data collection on the right & and Python web server on the left.
+# Script to start a new tab in a tmux session, launching the data collection on the right & and Python web server on the left.
 
 # Use https://bitbucket.org/lbesson/bin/src/master/.color.sh to add colors in Bash scripts
+echo "WARNING If you don't see colors correctly, remove the 'color.sh' file in 'uLogMe/scripts' to remove the colors, or modify it to suit your need (if you have a light background for instance)."  # See https://github.com/Naereen/uLogMe/issues/17
 [ -f color.sh ] && . color.sh
 
 echo -e "${yellow}Starting '${black}ulogme_tmux.sh'${white} ..."
@@ -24,18 +25,15 @@ fi
 
 port="${1:-8443}"  # Default is port=8124
 IP="${2:-localhost}"
-protocol="${3:-https}"
+protocol="${3:-http}"
 
 # Reference tmux man page (eg. https://linux.die.net/man/1/tmux)
 # start a new window,
 # name it ulogme
 tmux new-window -a -n 'uLogMe' "tmux split-window -d \"./ulogme_serve.sh ${port} ${IP} ${protocol} | tee /tmp/ulogme_serve_$$.log\" ; ./ulogme_data.sh | tee /tmp/ulogme_data_$$.log"
 # launch './ulogme_data.sh' in first one
-
 # split it half
-# launch './ulogme_serve.sh' in first one
-# tmux split-window -d "./ulogme_serve.sh ${port} ${IP} ${protocol} | tee /tmp/ulogme_serve_$$.log"
-# tmux rename-window 'uLogMe Server'
+# launch './ulogme_serve.sh' in second one
 
 sleep 12
 # return to current tab at the end
